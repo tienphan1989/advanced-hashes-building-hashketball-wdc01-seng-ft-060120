d91b2bbaa710c9d7c28c359f2002823ba24156d9
Tien Phan <tienphan1989@gmail.com>
2020-05-07 10:10:27 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -65,7 +65,7 @@ def game_hash
         assists: 1,
         steals: 2,
         blocks: 7,
-        slam_dunks: 2,
+        slam_dunks: 2
         },
         "Bismack Biyombo" => {
           number: 0,
