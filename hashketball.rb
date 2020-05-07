1b2424f86a4a8c99ad7006653935d3da3f0112a0
Tien Phan <tienphan1989@gmail.com>
2020-05-07 09:59:48 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -1,5 +1,5 @@
 def game_hash
-game_hash = {:home => {:team_name => "Brooklyn Nets", 
+game_hash = {{:home => {:team_name => "Brooklyn Nets", 
 :colors => ["Black", "White"], 
 :players => [
   {:player_name => "Alan Anderson", :number => "0", :shoe => "16", :points => "22", :rebounds => "12", :assists => "12", :steals => "3", :blocks => "1", :slam_dunks => "1"}, 
@@ -13,5 +13,5 @@ game_hash = {:home => {:team_name => "Brooklyn Nets",
 {:player_name => "Bismack Biyombo", :number => "0", :shoe => "16", :points => "12", :rebounds => "4", :assists => "7", :steals => "22", :blocks => "15", :slam_dunks => "10"},
 {:player_name => "Desagna Diop", :number => "2", :shoe => "14", :points => "24", :rebounds => "12", :assists => "12", :steals => "4", :blocks => "5", :slam_dunks => "5"},
 {:player_name => "Ben Gordon", :number => "8", :shoe => "15", :points => "33", :rebounds => "3", :assists => "2", :steals => "1", :blocks => "1", :slam_dunks => "0"},
-{:player_name => "Kemba Walker", :number => "33", :shoe => "15", :points => "6", :rebounds => "12", :assists => "12", :steals => "7", :blocks => "5", :slam_dunks => "12"}]}
+{:player_name => "Kemba Walker", :number => "33", :shoe => "15", :points => "6", :rebounds => "12", :assists => "12", :steals => "7", :blocks => "5", :slam_dunks => "12"}]}}
 end
